418b294be1248742dd18cc42ec51d782ede16bfd
Alexandr Korsak <alex.korsak@gmail.com>
2021-11-25 17:25:45 +0100

    add initial project

---
+++ lib/ecto_versions.ex
@@ -0,0 +1,18 @@
+defmodule EctoVersions do
+  @moduledoc """
+  Documentation for `EctoVersions`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoVersions.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_versions_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoVersionsTest do
+  use ExUnit.Case
+  doctest EctoVersions
+
+  test "greets the world" do
+    assert EctoVersions.hello() == :world
+  end
+end
